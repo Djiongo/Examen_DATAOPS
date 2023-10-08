01672c66ae93e81cc8bb2ff7ba602bf1a9eaa792
Djiongo <91948720+Djiongo@users.noreply.github.com>
2023-10-08 12:30:49 +0200

    first commit

---
+++ essaie.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+t= np.array([1,2,3])
+
+print (t)
+print(t.dtype)
+
+r=np.array(["big data","python","pandas"])
+
+print(r)
+print(r.dtype)
+
+r[0] ="bonjour tout le monde ca va?"
+print(r)
